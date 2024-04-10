5b15b3e547e78da87caa7cd51663d95be20c449b
Sravani Ramineni <musunurisravani34@gmail.com>
2024-04-10 16:54:26 +0200

    Add files via upload

---
+++ javascript_Session2/1_quataions.js
@@ -0,0 +1,17 @@
+//Double quotation inside string --\"double quoted"\
+
+var myStr = "I am a \"double quoted\"   string ";
+var mystring1="Iam second \"double quoted\" string";
+var mystring1="Iam second \'single quoted\'string";
+
+
+console.log(myStr);
+console.log(mystring1);
+//Quoting string with simgle quote-- 'I am a "single quoted" string'\
+
+var myStr2 = 'I am a "double quoted" string \\"iam sravani" \\"Iam laasya" \n "this is next line"\n "this is another next line"';
+var myStr3 = "I am a 'double quoted' string \t hello \t  Hi\b hello";
+
+
+console.log(myStr2);
+console.log(myStr3);
