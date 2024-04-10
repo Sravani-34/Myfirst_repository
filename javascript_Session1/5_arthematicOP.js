788b1604375d665cc8616c2395f95bd4e5dbeedf
Sravani Ramineni <musunurisravani34@gmail.com>
2024-04-10 16:53:39 +0200

    Add files via upload

---
+++ javascript_Session1/5_arthematicOP.js
@@ -0,0 +1,17 @@
+var x=20;
+var y=50;
+//Adition
+var sum=x+y;
+console.log(sum);
+//Substarction
+var sub=x-y;
+console.log(sub);
+//Multiplication
+var mul=x*y;
+console.log(mul);
+//Division
+var div=x/y;
+console.log(div);
+//Reminder
+var rem=x%y;
+console.log(rem);
